f03a6f3859052fcfb11e1ba4c977eda8e6e88207
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-05-30 08:51:28 +0900

    Update accform.js

--- blocks/accform/accform.js
+++ blocks/accform/accform.js
@@ -1,11 +1,38 @@
 export default async function decorate(block) {
     block.innerHTML = `
-    <h1> ACC form v2 </h1>
+    <h1> ACC form v3 </h1>
     <p>
-    HELLO acc form　v２ with EDS
-    <form method="POST" action="http://localhost:3333/text">
+    HELLO acc form v3 with EDS
+    <form id="inquiry" >
     <input type="text" name="edstext"/>
     <input type="submit" value="OK">
     </form>
     `;
+
+    const form = document.getElementById("inquiry");
+    form.addEventListener("submit", async ev => {
+        ev.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());    
+        console.log(data); // {name: 'test', email: 'test@example.com', content: 'xxx'}
+    
+        try {
+            // APIコール
+            await window.fetch("http://localhost:3333/text", {
+                method: "POST",
+                body: JSON.stringify(data),
+            });
+    
+            window.alert("送信しました。");
+    
+            // 完了時に入力値をクリア
+            form.reset();
+    
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+
+
 }
