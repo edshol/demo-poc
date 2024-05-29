cba53dc7fed54cd3b4fdbcf4432a742b79692496
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2024-05-30 08:43:45 +0900

    update

--- blocks/accform/accform.js
+++ blocks/accform/accform.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
     block.innerHTML = `
     <h1> ACC form v2 </h1>
     <p>
-    hello acc form with EDS
+    HELLO acc form　v２ with EDS
     <form action="http://localhost:3333/text">
     <input type="text" name="edstext"/>
     <submit>
